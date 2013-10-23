6ed4cdf1cf0c790f5364665966369cd03f45f7fa
Kevin Bradshaw <sugardood@gmail.com>
2013-10-23 11:16:02 +0200

    bonjour

--- shoot/main.lua
+++ shoot/main.lua
@@ -1,11 +1,14 @@
 function love.load(arg)
+	require ("bullet")
 	pos_x = 250
+	pos_y = 200
 end
 
 function love.update(dt)
-	pos_x = pos_x + (100 * dt)
+	pos_x = pos_x + (100 * dt)*love.joystick.getAxis(1, 1)
+	pos_y = pos_y + (100 * dt)*love.joystick.getAxis(1, 2)
 end
 
 function love.draw()
-	love.graphics.rectangle("fill", pos_x, 200, 150, 100)
+	love.graphics.rectangle("fill", pos_x, pos_y, 20, 20)
 end
